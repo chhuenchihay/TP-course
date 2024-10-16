079da09deab6f38c650265995147f54d07f6f524
CHHUEN CHIHAY <hay.mario168@gmail.com>
2024-10-16 17:43:36 +0700

    Fix project display

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <h2>This is my first VueJs project</h2>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
